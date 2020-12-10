811130f3f20a25a1d27a712c9844462e2f2f69a0
Jan Roces <rocesjg@gmail.com>
2020-12-10 02:13:57 -0800

    added links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import MainPage from "./components/MainPage";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <MainPage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="link-container">
+          <button
+            onClick={() => window.open("http://janroces.com/")}
+            className="link">
+            janroces.com
+          </button>
+          <button
+            onClick={() => window.open("https://github.com/JanRoces")}
+            className="link">
+            github.com/JanRoces
+          </button>
+        </div>
+        <MainPage />
+      </div>
+    );
+  }
+
+  openLink = (key) => {
+    console.log("key: ", key);
+  };
 }
 
 export default App;
